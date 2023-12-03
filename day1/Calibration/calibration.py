175d16a6aaddf58571a740b4a839e632a519d345
Katerina Wang <yusie@live.cn>
2023-12-02 21:19:08 -0700

    python ver added

---
+++ day1/Calibration/calibration.py
@@ -0,0 +1,50 @@
+def first_num(s):
+    num = 0
+    for i, c in enumerate(s):
+        i += 1
+        if not c.isdigit():
+            if "one" in s[0:i] or "eno" in s[0:i]:
+                num = 1
+                break
+            if "two" in s[0:i] or "owt" in s[0:i]:
+                num = 2
+                break
+            if "three" in s[0:i] or "eerht" in s[0:i]:
+                num = 3
+                break
+            if "four" in s[0:i] or "ruof" in s[0:i]:
+                num = 4
+                break
+            if "five" in s[0:i] or "evif" in s[0:i]:
+                num = 5
+                break
+            if "six" in s[0:i] or "xis" in s[0:i]:
+                num = 6
+                break
+            if "seven" in s[0:i] or "neves" in s[0:i]:
+                num = 7
+                break
+            if "eight" in s[0:i] or "thgie" in s[0:i]:
+                num = 8
+                break
+            if "nine" in s[0:i] or "enin" in s[0:i]:
+                num = 9
+                break
+        else:
+            if num == 0:
+                num = int(c)
+                break
+    return num
+
+
+def main():
+    num = 0
+    f = open("string", "r")
+    for ln in f:
+        num += first_num(ln) * 10 + first_num(ln[::-1])
+    f.close()
+    print(num)
+
+
+if __name__ == '__main__':
+    main()

17484682245c50e5930d6f816cc18d7b8ed511a9
Katerina Wang <yusie@live.cn>
2023-12-04 07:26:47 -0700

    delete unused lines

--- day4/Scratchcards/main.py
+++ day4/Scratchcards/main.py
@@ -1,6 +1,6 @@
 def win_number():
     s = 0
-    f = open("example.txt", "r")
+    f = open("cards", "r")
     for ln in f:
         win_lst = ln[ln.index(":") + 1:ln.index("|")].split()
         num_lst = ln[ln.index("|")+1:].split()
@@ -15,14 +15,11 @@ def win_number():
 def win_cards():
     card_memo = {i:1 for i in range(1, 224)}  # how many lines in the file, save time just count directly
     key = 1
-    f = open("example.txt", "r")
+    f = open("cards", "r")
     for ln in f:
         next_card = key + 1
         i = 0
-        if card_memo[key] == 0:
-            card_memo[key] = 1
-        else:
-            i += card_memo[key]  # get the current card number
+        i += card_memo[key]  # get the current card number
         win_lst = ln[ln.index(":") + 1:ln.index("|")].split()
         num_lst = ln[ln.index("|")+1:].split()
         for num in num_lst:
